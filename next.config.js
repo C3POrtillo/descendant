f4ab7937f03d8f6d0ecd08d045cd3a96c34cb908
Camilo III Ortillo <camortillo@gmail.com>
2024-07-25 17:40:38 -0600

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    WEAPON_JSON: process.env.WEAPON_JSON,
+    MODULE_JSON: process.env.MODULE_JSON,
+    STAT_JSON: process.env.STAT_JSON
+  },
+  images: {
+    domains: ['open.api.nexon.com']
+  }
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import type { FC } from 'react';
+
+import Container from '@/components/container/Container';
+import Header from '@/components/header/Header';
+
+const Index: FC = () => (
+  <>
+    <Header heading="Temp" />
+    <Container>Temp</Container>
+  </>
+);
+
+export default Index;
