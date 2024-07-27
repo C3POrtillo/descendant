5d51db09dda1a2dc78f63b8c673a1c94b42fb6ac
Camilo III Ortillo <camortillo@gmail.com>
2024-07-27 16:41:16 -0600

    add sublabel

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -2,23 +2,31 @@ import type { FC, ReactNode, TableHTMLAttributes } from 'react';
 
 export interface TableProps extends TableHTMLAttributes<HTMLTableElement> {
   label?: string;
+  sublabel?: string;
   headers?: ReactNode;
   body?: ReactNode;
 }
 
-const Table: FC<TableProps> = ({ label, headers, body, className, ...props }) => (
-  <fieldset
-    className={`table-wrapper rounded-xl border-2 border-solid border-white bg-slate-900 text-3xl shadow-xl shadow-black ${className}`}
-  >
-    <legend className="p-4 text-center text-6xl">{label}</legend>
-    {/* eslint-disable-next-line tailwindcss/migration-from-tailwind-2 */}
-    <table className="w-full overflow-clip rounded-xl" {...props}>
-      <thead>
-        <tr className="sticky-table">{headers}</tr>
-      </thead>
-      <tbody>{body}</tbody>
-    </table>
-  </fieldset>
-);
+const Table: FC<TableProps> = ({ label, sublabel, headers, body, className, ...props }) =>
+  headers &&
+  body && (
+    <fieldset
+      className={`table-wrapper rounded-xl border-2 border-solid border-white bg-slate-900 text-3xl shadow-xl shadow-black ${className}`}
+    >
+      {label && (
+        <legend className="p-4 text-center text-6xl">
+          {label}
+          {sublabel && <p className="text-yellow-200">{sublabel}</p>}
+        </legend>
+      )}
+      {/* eslint-disable-next-line tailwindcss/migration-from-tailwind-2 */}
+      <table className="w-full overflow-clip rounded-xl" {...props}>
+        <thead>
+          <tr className="sticky-table">{headers}</tr>
+        </thead>
+        <tbody>{body}</tbody>
+      </table>
+    </fieldset>
+  );
 
 export default Table;
