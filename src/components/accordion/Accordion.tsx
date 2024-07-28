cdccc3e0c1d92f08f2217d7642ebb67d79db487e
Camilo III Ortillo <camortillo@gmail.com>
2024-07-28 05:00:38 -0600

    formatting

--- src/components/accordion/Accordion.tsx
+++ src/components/accordion/Accordion.tsx
@@ -18,8 +18,8 @@ const Accordion: FC<AccordionProps> = ({ label, options }) => {
   return (
     <div className="relative inline-block w-full items-center justify-center rounded-xl px-4 py-1 text-left align-middle hover:bg-slate-400">
       <button onClick={toggleDropdown} className="inline-flex w-full justify-center rounded-md">
-        {label} 
-        <i className={['fa self-center ml-4', isOpen ? 'fa-chevron-up' : 'fa-chevron-down'].join(' ')} />
+        {label}
+        <i className={['fa ml-4 self-center', isOpen ? 'fa-chevron-up' : 'fa-chevron-down'].join(' ')} />
       </button>
       {isOpen && (
         <div className="absolute left-0 mt-3 flex min-h-max flex-col gap-4 rounded-xl border-2 border-solid border-white bg-slate-900 p-4">
