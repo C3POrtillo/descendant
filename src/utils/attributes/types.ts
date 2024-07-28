ddead8ffbfcb6c08a248a6853ae158cde3beda6c
Camilo III Ortillo <camortillo@gmail.com>
2024-07-27 18:45:40 -0600

    consistency

--- src/utils/attributes/types.ts
+++ src/utils/attributes/types.ts
@@ -1,16 +1,16 @@
 export const attributesArray = ['Non-attribute', 'Chill', 'Fire', 'Electric', 'Toxic'] as const;
 export type AttributesType = (typeof attributesArray)[number];
 
-type AttributeImages = {
+type AttributesImages = {
   attribute: string;
   fragment: string;
 }
 
-export const attributeImages = attributesArray.reduce((acc, attribute ) => {
+export const attributesImages = attributesArray.reduce((acc, attribute ) => {
   acc[attribute] = {
     attribute: `/assets/images/attribute/${attribute.toLowerCase()}.png`,
     fragment: `/assets/images/fragment/${attribute.toLowerCase()}.png`,
   }
 
   return acc
-}, {} as Record<AttributesType, AttributeImages>)
+}, {} as Record<AttributesType, AttributesImages>)
