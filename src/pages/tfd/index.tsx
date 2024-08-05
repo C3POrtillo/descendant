09d4eae8355aa3684c4bbac82df0a483cc206f3b
Camilo III Ortillo <camortillo@gmail.com>
2024-08-04 20:17:45 -0600

    Update index.tsx

--- src/pages/tfd/index.tsx
+++ src/pages/tfd/index.tsx
@@ -19,7 +19,7 @@ const Index: FC<IndexProps> = ({ seo }) => (
 
 export const getStaticProps = async () => {
   const title = 'The First Descendant (TFD) Helper Site';
-  const description = `Homepage for ortillo.cam/tfd, a helper website for The First Descendant (TFD). 
+  const description = `A helper website for The First Descendant (TFD). 
     Contains links to Pattern Wishlist, Void Shard/Void Fragment Data, Weapon DPS Data, 
     Effective Health Points (EHP) Calculator, External Component Data.`;
 
