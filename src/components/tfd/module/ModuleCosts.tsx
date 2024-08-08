819799485a241454c54513653663d84ab70bb37b
Camilo III Ortillo <camortillo@gmail.com>
2024-08-08 16:38:20 -0600

    refactor: change border to black

--- src/components/tfd/module/ModuleCosts.tsx
+++ src/components/tfd/module/ModuleCosts.tsx
@@ -24,7 +24,7 @@ const ModuleCosts: FC<ModuleCostsProps> = ({ label, kuiperPerLevel }) => {
       <div key={currentKuiper} className={['odd:bg-slate-800 even:bg-slate-700', gridClasses].join(' ')}>
         {values.map((value, valueIndex) => {
           const spanClass = valueIndex !== 0 ? 'col-span-2' : levelClasses;
-          const borderClass = valueIndex % 2 === 1 ? 'border-l-1 border-white px-2' : undefined;
+          const borderClass = valueIndex % 2 === 1 ? 'border-l-1 border-black px-2' : undefined;
 
           return (
             <span key={value} className={[spanClass, borderClass].filter(string => string).join(' ')}>
@@ -40,11 +40,13 @@ const ModuleCosts: FC<ModuleCostsProps> = ({ label, kuiperPerLevel }) => {
     const labelColor = index === 0 ? 'text-sky-400' : 'text-yellow-600';
 
     return (
-      <div key={cost} className="col-span-4 flex flex-col border-l-1 border-white">
-        <span className={['pl-2 border-b-1 border-white', labelColor].join(' ')}>{cost}</span>
+      <div key={cost} className="col-span-4 flex flex-col border-l-1 border-black">
+        <span className={['pl-2', labelColor].join(' ')}>{cost}</span>
         <div className="grid grid-cols-2">
           {costSublabels.map((sublabel, sublabelIndex) => (
-            <span key={sublabel} className={sublabelIndex === 0 ? 'px-2': undefined}>{sublabel}</span>
+            <span key={sublabel} className={sublabelIndex === 0 ? 'px-2' : undefined}>
+              {sublabel}
+            </span>
           ))}
         </div>
       </div>
@@ -55,7 +57,7 @@ const ModuleCosts: FC<ModuleCostsProps> = ({ label, kuiperPerLevel }) => {
     // eslint-disable-next-line tailwindcss/migration-from-tailwind-2
     <div className="flex flex-col overflow-clip rounded-lg border-2 border-black bg-slate-900 shadow-lg shadow-black">
       <h3 className={['py-2 text-center text-lg xl:text-xl', labelClass].join(' ')}>{label}</h3>
-      <div className={['border-t-1 border-white', gridClasses].join(' ')}>
+      <div className={['border-y-1 border-black', gridClasses].join(' ')}>
         <span className={['self-end', levelClasses].join(' ')}>{'Lvl.'}</span>
         {headers}
       </div>
