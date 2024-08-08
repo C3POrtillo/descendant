296a270d1425baa11e97487dd3d44cec32362cdd
Camilo III Ortillo <camortillo@gmail.com>
2024-08-07 20:52:19 -0600

    refactor: update font size

--- src/components/tfd/patterns/PatternHeaders.tsx
+++ src/components/tfd/patterns/PatternHeaders.tsx
@@ -17,7 +17,7 @@ const formatData = (array: readonly (string | number)[]): HeadersType[] =>
         <div key={data} className={['flex flex-row items-center gap-4 px-4 py-1 md:py-4'].join(' ')}>
           <div className="text-lg lg:text-2xl">{data}</div>
           {isNumber && (
-            <div className="flex w-1/2 flex-col text-nowrap text-sm">
+            <div className="flex w-1/2 flex-col text-nowrap text-sm lg:text-base">
               <div className="text-left">Expected: {expectedAttempts}</div>
               <div className="text-left">
                 Nearly Guaranteed: {nearlyGuaranteed} ± {nearlyGuaranteedRange}
