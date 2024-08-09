d13cf599482593bc7affa8e2464879ea0d9cbd6f
Camilo III Ortillo <camortillo@gmail.com>
2024-08-08 18:23:33 -0600

    refactor: wrap in container

--- src/components/tfd/header/Breadcrumbs.tsx
+++ src/components/tfd/header/Breadcrumbs.tsx
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router';
 
 import type { FC } from 'react';
 
+import Container from '@/components/container/Container';
 import TFDLink from '@/components/tfd/header/Link';
 import { breadcrumbLabels } from '@/utils/paths';
 import { titleCase } from '@/utils/utils';
@@ -19,23 +20,25 @@ const Breadcrumbs: FC<BreadcrumbsProps> = ({ slug }) => {
   }
 
   return (
-    <div className="mx-4 flex flex-row gap-2 lg:mx-16 2xl:mx-24">
-      {paths.map((path, index) => {
-        const label = breadcrumbLabels[path] || titleCase(path);
+    <Container className="m-0 justify-start">
+      <div className="flex flex-row gap-2 lg:mx-16 2xl:mx-24">
+        {paths.map((path, index) => {
+          const label = breadcrumbLabels[path] || titleCase(path);
 
-        return (
-          <span key={path}>
+          return (
+            <span key={path}>
             /{' '}
-            <TFDLink
-              className="text-hover bg-hover rounded-md px-2 py-1"
-              label={label}
-              path={`/${paths.join('/')}`}
-              disabled={index === paths.length - 1}
-            />
-          </span>
-        );
-      })}
-    </div>
+              <TFDLink
+                className="text-hover bg-hover rounded-md px-2 py-1"
+                label={label}
+                path={`/${paths.join('/')}`}
+                disabled={index === paths.length - 1}
+              />
+            </span>
+          );
+        })}
+      </div>
+    </Container>
   );
 };
 
