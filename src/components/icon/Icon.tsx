776e1641cffd17fcdaacdf5f7535411e7dea354c
Camilo III Ortillo <camortillo@gmail.com>
2024-08-09 01:20:37 -0600

    feat: add className to div wrapper

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -7,14 +7,15 @@ export interface IconProps {
   alt?: string;
   backgroundClass?: 'diamond' | 'hexagon' | 'rounded-square' | 'rhombus';
   size?: '6' | '8' | '10';
+  className?: string;
 }
 
-const Icon: FC<IconProps> = ({ src, alt, backgroundClass, size = '8' }) => {
+const Icon: FC<IconProps> = ({ src, alt, backgroundClass, size = '8', className }) => {
   const getSize = () => `size-${size} `;
 
   return (
     src && (
-      <div>
+      <div className={className}>
         <div className={['relative flex min-h-6 min-w-6 flex-wrap', getSize()].join(' ')}>
           {backgroundClass && <div className={backgroundClass} />}
           <Image fill={true} sizes="100%" src={src} alt={alt || ''} quality={100} loading="lazy" />
